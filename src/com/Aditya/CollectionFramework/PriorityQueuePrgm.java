6b5aeb0477e4ebd8d69dc7a5d1fe8ac962ec2409
Aditya10403 <adityashukla@gmail.com>
2023-12-20 07:33:57 +0530

    Added on 20-12-23

---
+++ src/com/Aditya/CollectionFramework/PriorityQueuePrgm.java
@@ -0,0 +1,32 @@
+package com.Aditya.CollectionFramework;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueuePrgm {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Comparator.reverseOrder());
+        pQ.add(1);
+        pQ.add(5);
+        pQ.add(2);
+        pQ.add(9);
+        pQ.remove();
+        System.out.println(pQ);
+
+        Queue<Integer> q = new PriorityQueue<>(
+          new Comparator<Integer>(){
+                public int compare(Integer i1,Integer i2){
+                    return i2.compareTo(i1);
+                }
+          });
+        q.add(4);
+        q.add(1);
+        q.add(9);
+        q.add(5);
+        System.out.println(q);
+        q.poll();
+        System.out.println(q);
+    }
+}
---
+++ src/com/Aditya/programs/MissingNo.java
@@ -0,0 +1,33 @@
+package com.Aditya.programs;
+
+import java.util.Arrays;
+
+public class MissingNo {
+    public static void main(String[] args) {
+        int[] arr = {3, 0 ,1};
+        System.out.println(missing(arr));
+    }
+    static int missing(int[] nums){
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]){
+                swap(nums, i ,correct);
+            }
+            else {
+                i++;
+            }
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j){
+                return j;
+            }
+        }
+        return nums.length;
+    }
+    static void swap(int[] nums, int index, int correct){
+        int tmp = nums[index];
+        nums[index] = nums[correct];
+        nums[correct] = tmp;
+    }
+}
