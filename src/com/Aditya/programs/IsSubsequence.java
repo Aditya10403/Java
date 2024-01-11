79c5943c0fac4a1e12e356b0d963f82876f2191b
Aditya10403 <adityashukla@gmail.com>
2024-01-11 09:16:47 +0530

    added program

--- src/com/Aditya/programs/IsSubsequence.java
+++ src/com/Aditya/programs/IsSubsequence.java
@@ -1,8 +1,22 @@
 package com.Aditya.programs;
 
+import java.util.Scanner;
+
 public class IsSubsequence {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the floor area of the room(m*m)");
+        double m = in.nextInt();
+        double n = in.nextInt();
+        System.out.println("Enter the plant area of a single plant(in cm2)");
+        double a = in.nextInt();
+        double c = (m * n / a) * 100 * 100;
+        int ans = (int) (c / 10) * 10;
+        System.out.println("Total plants placed on floor area "
+                +String.format("%.2f",m) +" * " +String.format("%.2f",n) +
+                " is "+ ans +" plants produces "
+                +String.format("%.2f",ans*0.9)  +
+                " litres of oxygen in a day");
     }
     public boolean isSubsequence(String s, String t) {
         int i = 0;
