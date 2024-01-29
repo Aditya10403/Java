4ce8010eaf29e272370bd06be22d47d6e581b928
Aditya10403 <adityashukla@gmail.com>
2024-01-29 08:26:02 +0530

    added DFS programs

--- src/com/Aditya/DataStructureAndAlgorithm/DataStructures/Trees/DepthFirstSearch.java
+++ src/com/Aditya/DataStructureAndAlgorithm/DataStructures/Trees/DepthFirstSearch.java
@@ -33,4 +33,34 @@ public class DepthFirstSearch {
 
         return Math.max(leftHeight, rightHeight) + 1;
     }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, start, mid);
+        node.right = sortedArrayToBST(nums, mid+1, end);
+        return node;
+    }
 }
