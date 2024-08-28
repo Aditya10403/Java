d9238ca1e19bbbed5085366ac9e594f7c12b192c
Aditya10403 <adityashukla@gmail.com>
2024-08-28 06:48:43 +0530

    add programs on 28-08-24

--- src/com/aditya/programs/LinkedListQuestions.java
+++ src/com/aditya/programs/LinkedListQuestions.java
@@ -1,6 +1,6 @@
 package com.aditya.programs;
 
-public class DeleteMiddle {
+public class LinkedListQuestions {
     public static void main(String[] args) {
         ListNode l = new ListNode(); // 1,3,4,1,2,6
     }
@@ -25,4 +25,22 @@ public class DeleteMiddle {
         return head;
     }
 
+    /**
+     * i/p -> 2,1,3,5,6,4,7 <br>
+     * o/p -> 2,3,6,7,1,5,4
+     */
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) return head;
+        ListNode i = head;
+        ListNode evenStart = head.next;
+        ListNode j = head.next;
+        while (i.next != null && j.next != null) {
+            i.next = i.next.next;
+            j.next = j.next.next;
+            i = i.next;
+            j = j.next;
+        }
+        i.next = evenStart;
+        return head;
+    }
 }
